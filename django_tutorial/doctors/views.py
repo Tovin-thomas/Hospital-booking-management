af9493f7737af7e61fa639f30e39b1068daed65d
Tovin-thomas <tovinthomas2@gmail.com>
2026-01-21 14:08:51 +0530

    here addedd a feacture  wheredocotors are shown based on the department in the department and in the doctors page

--- django_tutorial/doctors/views.py
+++ django_tutorial/doctors/views.py
@@ -6,8 +6,21 @@ from .forms import AvailabilityForm, LeaveForm
 from bookings.models import Booking
 
 def doctors(request):
+    # Get department filter from URL parameter
+    department_id = request.GET.get('department', None)
+    selected_department = None
+    
+    # Filter doctors by department if specified
+    if department_id:
+        doctors_list = Doctors.objects.filter(dep_name_id=department_id).prefetch_related('availabilities', 'leaves')
+        selected_department = get_object_or_404(Departments, id=department_id)
+    else:
+        doctors_list = Doctors.objects.all().prefetch_related('availabilities', 'leaves')
+    
     dict_docs = {
-        'doctors': Doctors.objects.all().prefetch_related('availabilities', 'leaves')
+        'doctors': doctors_list,
+        'selected_department': selected_department,
+        'all_departments': Departments.objects.all(),
     }
     return render(request, 'doctors.html', dict_docs)
 
